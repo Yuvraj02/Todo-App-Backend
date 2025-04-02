9ace16816d83220d227d64e7de7144508e1570f4
Yuvraj <yuvrajsinghbhadoria@gmail.com>
2025-04-02 17:16:28 +0530

    Backend Connected with Frontend

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import express from "express"
-import path from 'path'
+import path from "path"
 
 const port = 3000
 const app = express();
 
-const appPath = "E:\\React\\Todo App"
+app.use(express.static(path.join(__dirname, "../Todo App/dist")))
 
 app.get("/",(req,res)=> {
-    res.send(`<h1> Server Running Successfully </h1>`);
     console.log("GET METHOD initiated")
+    res.sendFile(path.join(__dirname, "../Todo App/dist/index.html"))
 });
 
 app.listen(port, ()=>{
     console.log(`Listening On Port ${port}`)
 })
-
